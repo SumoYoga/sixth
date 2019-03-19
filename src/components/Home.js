204262396f6b61e390ad7270121cd0321e2af307
Michael L Petersen <ekmlpe@danskespil.dk>
2019-03-19 11:00:52 +0100

    circle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import Markdown from 'react-markdown'
-import logo from '../assets/sixth.svg'
+import logo from '../assets/sixth-circle.svg'
 import '../styles/Home.scss'
-import Background from './Background'
 
-const md = `
-# SIXTH &middot; FRONTEND UDVIKLING
-
-CVR 38418998
-`
+const md = `hello@sixth.dk`
 
 const Index = () => {
   return (
     <div className="Home">
-      <Background />
       <header className="header">
         <img src={logo} className="logo" alt="logo" />
         <Markdown source={md} />
