2144fbfd4800c5fc3d6559f12d76a89b646b8ab6
Mike Lothar <mikelothar@gmail.com>
2019-12-03 20:33:26 +0100

    next

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Head from 'next/head'
+// import Nav from '../components/nav'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Sixth</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#F8E81C"></meta>
+      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"></meta>
+    </Head>
+
+    <img src="/sixth.svg" alt="Sixth"/>
+
+
+    <style jsx>{`
+
+    
+    div {
+height: 90vh;
+display: flex;
+align-items: center;
+justify-content: center;
+      }
+
+      img {
+        min-height: 200px;
+
+height: 25vmin;
+      }
+
+      
+    `}</style>
+  </div>
+)
+
+export default Home
