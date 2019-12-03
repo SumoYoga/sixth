515521b3e2277f44b37cd01bfbed711752665102
Mike Lothar <mikelothar@gmail.com>
2019-12-03 21:06:56 +0100

    clean up

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,33 @@ const Home = () => (
       <title>Sixth</title>
       <link rel="icon" href="/favicon.ico" />
       <meta name="theme-color" content="#F8E81C"></meta>
-      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"></meta>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no"
+      ></meta>
     </Head>
 
-    <img src="/sixth.svg" alt="Sixth"/>
-
+    <img src="/sixth.svg" alt="Sixth" />
 
     <style jsx>{`
+      :global(body) {
+        margin: 0;
+        height: 100%;
+        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+          Helvetica, sans-serif;
+      }
 
-    
-    div {
-height: 90vh;
-display: flex;
-align-items: center;
-justify-content: center;
+      div {
+        height: 90vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
       }
 
       img {
         min-height: 200px;
-
-height: 25vmin;
+        height: 25vmin;
       }
-
-      
     `}</style>
   </div>
 )
