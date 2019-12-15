bada7989f2b5f35b0381290a5b73e7685281fb1d
Mike Lothar <mikelothar@gmail.com>
2019-12-15 20:18:21 +0100

    bulma

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,54 @@
 import React from 'react'
 import Head from 'next/head'
+import '../sass/styles.scss'
 // import Nav from '../components/nav'
 
 const Home = () => (
   <div>
     <Head>
       <title>Sixth</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico"/>
       <meta name="theme-color" content="#F8E81C"></meta>
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1,shrink-to-fit=no"
-      ></meta>
+      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"></meta>
     </Head>
 
     <div className="container">
-      <img src="/sixth.svg" alt="Sixth" />
+      <img src="/sixth.svg" alt="Sixth"/>
       <h1>Sixth</h1>
     </div>
+
+
+    <h1 className="title">
+      Sixth
+    </h1>
+
+    <p className="subtitle">
+      Modern CSS framework based
+      on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+    </p>
+
 
     <style jsx>{`
       :global(*) {
         box-sizing: border-box;
       }
-      :global(body) {
-        margin: 0;
-        padding: 20px;
-        height: 100%;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
+      //:global(body) {
+      //  margin: 0;
+      //  padding: 20px;
+      //  height: 100%;
+      //  font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+      //    Helvetica, sans-serif;
+      //}
 
-      .container {
-        background: #F4F8FB;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        padding: 50px 20px;
-        border-radius: 25px;
-        height: calc(100vh - 40px);
-      }
+      //.container {
+      //  background: #F4F8FB;
+      //  width: 100%;
+      //  display: flex;
+      //  flex-direction: column;
+      //  padding: 50px 20px;
+      //  border-radius: 25px;
+      //  height: calc(100vh - 40px);
+      //}
 
       img {
         height: 150px;
@@ -47,9 +56,9 @@ const Home = () => (
       }
 
       h1 {
-        font-size: 21px;
-        font-weight: 400;
-        text-align: center;
+        //font-size: 21px;
+        //font-weight: 400;
+        //text-align: center;
         text-transform: uppercase;
         letter-spacing: 0.6em;
       }
